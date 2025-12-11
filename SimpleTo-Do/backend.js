5aefae9966550e703387876a0be46b49f3462e16
Mark Chaitra <96210444+MarkChaitra@users.noreply.github.com>
2025-12-11 16:27:00 -0500

    Add files via upload
    added first simple ToDo list website

---
+++ SimpleTo-Do/backend.js
@@ -0,0 +1,37 @@
+function loadList(){
+
+    let list = getList();
+
+    let html = "";
+    for(let i = 0; i < list.length; i++){
+        html += `<div class="List-tasks">${list[i]}<i onclick="removeItem('${list[i]}')" class="bi bi-trash-fill"></i></div>`;
+    }
+    document.getElementsByClassName("List-Items")[0].innerHTML = html;
+    document.getElementsByClassName("Tasks-remaining")[0].innerHTML = "You have " + list.length + " tasks";
+}
+
+function addToList(item) {
+
+    if(item.trim() === ""){return;}
+
+    let list = getList()
+    list.push(item);
+    localStorage.setItem("myList", JSON.stringify(list));
+
+    document.getElementById("user-input").value = "";
+
+    loadList();
+}
+
+function getList() {
+    return JSON.parse(localStorage.getItem("myList")) || [];
+}
+
+function removeItem(item){
+    let list = getList();
+    list = list.filter(items => items !== item);
+    localStorage.setItem("myList", JSON.stringify(list));
+
+    loadList();
+}
+  
